18e74f770a5d5f04d51bb0156b6842550f1807d5
MMarsolek <mamarsolek@gmail.com>
2022-03-08 13:00:25 -0800

    Storing changes for pull

--- src/components/pages/Characters.js
+++ src/components/pages/Characters.js
@@ -4,15 +4,18 @@ import { Link, Route, Routes } from 'react-router-dom'
 import CharacterPage from "./CharacterPage";
 
 
-
 export default function  Characters() {
     //Make axios request to get data from backend
     //For each character, return image card.
     const [characters, setCharacter] = useState([{}]);
+    const [loading, setLoading] = useState(true)
     useEffect(async () => {
         let response;
         try{
             response = await axios.get('https://fierce-crag-37779.herokuapp.com/api/characters');
+            if (loading === true){
+                setTimeout(()=>setLoading(false), 1500
+                )}
         }catch(err){
             console.log(err);
             throw err;
@@ -20,24 +23,25 @@ export default function  Characters() {
         setCharacter(response.data);
     },[]);
      return (
-            <>
-                {
-                    characters.map((character, index)=>{
-                    return(
-                        <div key={index} className="card">
-                            <Link to={`/characters/${character.name}`} className="btn">
-                            <img className="card-img-top" src={character.portrait} alt="Card image cap"></img>
-                            <div className="card-body">
-                                <Routes>
-                                    <Route path={`/characters/${character.name}`} render={(character) => <CharacterPage  />} />
-                                </Routes>
-                                {character.name}
-                                </div>
-                                </Link>
+        <>        
+            {
+                characters.map((character, index)=>{
+                return(
+                    <div key={index} className="card">
+                        <Link to={`/characters/${character.name}`} className="btn">
+                        <img className="card-img-top" src={character.portrait} alt="Card image cap"></img>
+                        <div className="card-body">
+                            <Routes>
+                                <Route path={`/characters/${character.name}`} render={(character) => <CharacterPage  />} />
+                            </Routes>
+                            {character.name}
                             </div>
-                        ) 
-                    })
-                }
+                            </Link>
+                        </div>
+                    ) 
+                })
+            }
             </>
         );
 }
+

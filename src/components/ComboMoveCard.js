8d37d3583abdd5a2094e0a17ef7e76f12aa48284
carterfm <carterf.morfitt@gmail.com>
2022-03-10 15:23:01 -0800

    Fixed an aspect of the text input

--- src/components/ComboMoveCard.js
+++ src/components/ComboMoveCard.js
@@ -3,14 +3,16 @@ import React from 'react';
 // key={step.id} name={step.name} image={step.image} input={step.input} stepNumber={step.stepNumber}
 function ComboMoveCard ({name, image, input, stepNumber}) {
   return (
-    <div className="combo-move-card card col-4 col-md-3 m-2 d-flex align-items-center">
-      <h1>{stepNumber}</h1>
-      <img className="card-img" src={image} alt="Card image cap"></img>
-      <div className="text-holder">
-        <h4>{name}</h4>
-        {input && (
-          <p>({input})</p>
-        )}
+    <div className="combo-move-card card col-4 col-md-3 col-lg-2 m-2 d-flex align-items-center">
+      <div className="inside-card">
+        <h1>{stepNumber}</h1>
+        <img className="card-img" src={image} alt="Card image cap"></img>
+        <div className="text-holder">
+          <p>{name}</p>
+          {input && (
+            <p>({input})</p>
+          )}
+        </div>
       </div>
     </div>
   );
